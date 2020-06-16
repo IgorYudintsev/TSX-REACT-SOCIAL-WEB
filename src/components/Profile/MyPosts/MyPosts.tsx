1593684220917ee404dae91d0aa9bff36c728e65
mobile08092018 <kakvospitat@gmail.com>
2020-06-16 18:58:36 +0300

    16.06.2020-new folders

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,19 +3,20 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 
-
-
 const MyPosts = () => {
     return (
-        <div>My posts
+        <div className={s.postsBlock}>
+            <h2>My posts</h2>
             <div>
                 <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
 
             <div className={s.posts}>
-                <Post message='Hi.How are you?'  likesCount='0' />
-                <Post message='It`s my first post' likesCount='23' />
+                <Post message='Hi.How are you?' likesCount='0'/>
+                <Post message='It`s my first post' likesCount='23'/>
 
             </div>
         </div>
