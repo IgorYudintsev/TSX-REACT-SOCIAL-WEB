49418648d052bc633a6c43c058f7a9ce24d3fb51
mobile08092018 <kakvospitat@gmail.com>
2020-08-22 14:04:58 +0300

    22.08.2020-DialogsMessage

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,36 +1,59 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import {sendMessageCreator, StoreType, updateNewMessageBodyCreator} from './../../redux/state'
 
-type dialogs={
-    id:number,
-    name:string
+type dialogs = {
+    id: number,
+    name: string
 }
-
 type messages = {
-    id:number,
-    message:string
+    id: number,
+    message: string
 }
+// type generalType = {
+//     dialogs: Array<dialogs>,
+//     messages: Array<messages>
+//     newMessageBody: string
+// }
 
-type generalType={
-    dialogs:Array<dialogs>,
-    messages:Array<messages>
-}
+type DialogGeneralState = {
+    store: StoreType
 
-type DialogGeneralState={
-    AppGeneralStateForDialogs:generalType
 }
 
 let Dialogs = (props: DialogGeneralState) => {
+
+
+    let state = props.store.getState().dialogsPage
+
+    let newMessageBody = state.newMessageBody
+    let onSendMessageClick = () => {
+        props.store.dispatch(sendMessageCreator())
+    }
+    let onNewMessageChange = (event:ChangeEvent<HTMLTextAreaElement>) => {
+        let body = event.currentTarget.value;
+        props.store.dispatch(updateNewMessageBodyCreator(body))
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                          {props.AppGeneralStateForDialogs.dialogs.map(m => <DialogItem name={m.name} id={m.id}/>)}
+                {state.dialogs.map(m => <DialogItem name={m.name} id={m.id}/>)}
             </div>
 
             <div className={s.messages}>
-                {props.AppGeneralStateForDialogs.messages.map(m => <Message message={m.message} id={m.id}/>)}
+                <div>{state.messages.map(m => <Message message={m.message} id={m.id}/>)}</div>
+                <div>
+                    <div><textarea
+                        value={newMessageBody}
+                        placeholder={'Enter your message'}
+                        onChange={onNewMessageChange}
+                    ></textarea></div>
+                    <div>
+                        <button onClick={onSendMessageClick}>Send</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,4 +1,160 @@
+// import React from 'react';
+//
+// const addPost = 'ADD-POST';
+// const updateNewPostText = 'UPDATE-NEW-POST-TEXT';
+// const updateNewMessageBody = 'update-New-Message-Body';
+// const SendMessage = 'Send-Message';
+//
+// export type idialogs = {
+//     id: number,
+//     name: string
+// }
+// export type imessages = {
+//     id: number,
+//     message: string
+// }
+// export type idialogsPage = {
+//     messages: Array<imessages>
+//     dialogs: Array<idialogs>,
+//     newMessageBody: string
+//
+// }
+// export type iposts = {
+//     id: number,
+//     message: string,
+//     likesCount: number
+// }
+// export type iprofilePage = {
+//     posts: Array<iposts>
+//     newPostText: string
+// }
+// export type isidebar = {}
+// export type istate = {
+//     profilePage: iprofilePage,
+//     dialogsPage: idialogsPage,
+//     sidebar: isidebar
+// }
+// export type StoreType = {
+//     _state: istate
+//     _callSubscriber: (state: istate) => void
+//     subscribe: (observer: (state: istate) => void) => void
+//     getState: () => istate
+//     dispatch: (action: ActionsTypes) => void
+// }
+// type AddPostActionType = {
+//     type: 'ADD-POST',
+// }
+// type updateNewPostTextActionType = {
+//     type: 'UPDATE-NEW-POST-TEXT',
+//     newText: string
+// }
+//
+// export type ActionsTypes = AddPostActionType | updateNewPostTextActionType | updateNewMessageBodyType | SendMessageType
+//
+// let store: StoreType = {
+//     _state: {
+//         profilePage: {
+//             posts: [
+//                 {id: 1, message: 'Hi', likesCount: 10},
+//                 {id: 2, message: 'How are you?', likesCount: 100},
+//             ],
+//             newPostText: 'it-kamasutra.com'
+//         },
+//         dialogsPage: {
+//             messages: [
+//                 {id: 1, message: 'Hi'},
+//                 {id: 2, message: 'How are you?'},
+//                 {id: 3, message: 'Yoo'},
+//                 {id: 4, message: 'How are you?'},
+//                 {id: 5, message: 'Yoo'},
+//             ],
+//             dialogs: [
+//                 {id: 1, name: 'Dimach'},
+//                 {id: 2, name: 'Sasha'},
+//                 {id: 3, name: 'Igor'},
+//                 {id: 4, name: 'Olga'},
+//                 {id: 5, name: 'Nadzeika'},
+//             ],
+//             newMessageBody: ''
+//         },
+//         sidebar: {}
+//     },
+//     _callSubscriber(state: istate) {
+//         console.log('rerenderEntireTree');
+//     },
+//     getState() {
+//         return this._state;
+//     },
+//     subscribe(observer: (state: istate) => void) {
+//         this._callSubscriber = observer
+//     },
+//
+//     dispatch(action) {
+//         if (action.type === addPost) {
+//             let newPost = {
+//                 id: 5,
+//                 message: this._state.profilePage.newPostText,
+//                 likesCount: 0
+//             }
+//             this._state.profilePage.posts.push(newPost);
+//             this._state.profilePage.newPostText = '';
+//             this._callSubscriber(this._state)
+//         } else if (action.type === updateNewPostText) {
+//             this._state.profilePage.newPostText = action.newText
+//             this._callSubscriber(this._state);
+//         } else if (action.type == updateNewMessageBody) {
+//             this._state.dialogsPage.newMessageBody = action.body;
+//             this._callSubscriber(this._state);
+//         } else if (action.type = SendMessage) {
+//             let body = this._state.dialogsPage.newMessageBody;
+//             this._state.dialogsPage.newMessageBody = '';
+//             this._state.dialogsPage.messages.push({id: 6, message: body});
+//             this._callSubscriber(this._state)
+//         }
+//     }
+// }
+//
+// export let addPostActionCreator = () => {
+//     return {
+//         type: addPost
+//     }
+// }
+// export let newPostElementCreator = (text: string) => {
+//     return {
+//         type: updateNewPostText,
+//         newText: text
+//
+//     }
+// }
+//
+//
+// type SendMessageType = {
+//     type: typeof SendMessage
+// }
+// export let sendMessageCreator = (): SendMessageType => {
+//     return {
+//         type: SendMessage
+//     }
+// }
+//
+// type updateNewMessageBodyType = {
+//     type: typeof updateNewMessageBody
+//     body: string
+// }
+// export let updateNewMessageBodyCreator = (body: string):updateNewMessageBodyType => {
+//     return {
+//         type: updateNewMessageBody,
+//         body: body
+//     }
+// }
+//
+// export default store;
+//===================================
 import React from 'react';
+const addPost = 'ADD-POST';
+const updateNewPostText = 'UPDATE-NEW-POST-TEXT';
+const updateNewMessageBody = 'update-New-Message-Body';
+const SendMessage = 'Send-Message';
 
 export type idialogs = {
     id: number,
@@ -11,6 +167,8 @@ export type imessages = {
 export type idialogsPage = {
     messages: Array<imessages>
     dialogs: Array<idialogs>,
+    newMessageBody:string
+
 }
 export type iposts = {
     id: number,
@@ -41,10 +199,18 @@ type updateNewPostTextActionType = {
     type: 'UPDATE-NEW-POST-TEXT',
     newText:string
 }
-export type ActionsTypes=AddPostActionType|updateNewPostTextActionType
 
-const addPost = 'ADD-POST';
-const updateNewPostText = 'UPDATE-NEW-POST-TEXT';
+type updateNewMessageBodyType={
+    type: 'update-New-Message-Body'
+    body:string
+}
+
+type SendMessageType={
+    type: 'Send-Message'
+}
+
+export type ActionsTypes=AddPostActionType|updateNewPostTextActionType|updateNewMessageBodyType|SendMessageType
+
 let store: StoreType = {
     _state: {
         profilePage: {
@@ -69,6 +235,7 @@ let store: StoreType = {
                 {id: 4, name: 'Olga'},
                 {id: 5, name: 'Nadzeika'},
             ],
+            newMessageBody:''
         },
         sidebar: {}
     },
@@ -96,21 +263,40 @@ let store: StoreType = {
         else if(action.type===updateNewPostText){
             this._state.profilePage.newPostText = action.newText
             this._callSubscriber(this._state);
+        }else if(action.type==updateNewMessageBody){
+            this._state.dialogsPage.newMessageBody=action.body;
+            this._callSubscriber(this._state);
+        }else if(action.type=SendMessage){
+            let body=this._state.dialogsPage.newMessageBody;
+            this._state.dialogsPage.newMessageBody='';
+            this._state.dialogsPage.messages.push({id: 6, message:body});
+            this._callSubscriber(this._state)
         }
     }
 }
 
 export let addPostActionCreator=()=>{
-       return{
+    return{
         type: addPost
     }
 }
 export let newPostElementCreator = (text:string) => {
-
     return {
         type: updateNewPostText,
         newText: text
 
+    }
+}
+
+export let sendMessageCreator = ():SendMessageType => {
+    return {
+        type: SendMessage
+    }
+}
+export let updateNewMessageBodyCreator = (body:string):updateNewMessageBodyType => {
+    return {
+        type: updateNewMessageBody,
+        body: body
     }
 }
 
