fd92ce3e19449b861fb4a5061bf9b36549bb8d7a
mobile08092018 <kakvospitat@gmail.com>
2020-11-19 20:57:26 +0300

    19.11.2020 61 - React JS - cookie, login в теории, auth/me

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -3,12 +3,14 @@ import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sideBarReducer from "./sideBar-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sideBarReducer,
-    usersPage:usersReducer
+    usersPage:usersReducer,
+    auth:authReducer
 });
 export type AppStateType = ReturnType<typeof reducers>//это типизация state всего приложения
 let store: Store = createStore(reducers);//типизация из redux для Store
