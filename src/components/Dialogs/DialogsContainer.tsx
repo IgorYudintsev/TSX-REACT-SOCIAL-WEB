9143fb4e84be68b54b121c31765193196f2e4ba8
mobile08092018 <kakvospitat@gmail.com>
2021-04-16 18:25:30 +0300

    16.04.2021 76 - React JS - redux-form опять про базовые вещи

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -3,32 +3,29 @@ import {compose, Dispatch} from 'redux';
 import {sendMessageCreator, updateNewMessageBodyCreator} from './../../redux/dialogs-reducer'
 import Dialogs, {DialogGeneralState} from "./Dialogs";
 import {connect} from "react-redux";
-import { istate} from "../../redux/store";
+import {istate} from "../../redux/store";
 import {initialStateType} from "../../redux/auth-reducer";
 import {AppStateType} from "../../redux/redux-store";
 import {Redirect} from "react-router";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 
-type DialogsContainerType={
-    state:istate
-    auth:initialStateType
+type DialogsContainerType = {
+    state: istate
+    auth: initialStateType
 }
 
-
-const mapStatetoProps=(state:AppStateType)=>{
-      return{
-        dialogsPage:state.dialogsPage,
-          //теперь это не нужно
-        //isAuth: state.auth.isAuth
-          }
+const mapStatetoProps = (state: AppStateType) => {
+    return {
+        dialogsPage: state.dialogsPage,
+    }
 }
 
-const mapDispatchToProps=(dispatch:Dispatch)=>{
-    return{
-        sendMessage:()=>{
-             dispatch(sendMessageCreator())
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        sendMessage: () => {
+            dispatch(sendMessageCreator())
         },
-        updateNewMessageBody:(body:string)=>{
+        updateNewMessageBody: (body: string) => {
             dispatch(updateNewMessageBodyCreator(body))
         }
     }
