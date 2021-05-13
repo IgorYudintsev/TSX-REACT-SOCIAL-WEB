e205e3056e5dceb70f8289db82885f2916f0dc00
mobile08092018 <kakvospitat@gmail.com>
2021-05-13 18:37:40 +0300

    13.05.2021 83 - React JS - подключаем reselect (reselect часть 3)

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,26 +1,39 @@
 import {AppStateType} from "./redux-store";
+import {createSelector} from 'reselect';
+import {usersType} from "./users-reducer";
 
-export const GetUsers=(state:AppStateType)=>{
+//было
+// export const GetUsers=(state:AppStateType)=>{
+//     return state.usersPage.users;
+// }
+
+//получаем юзерсов
+const GetUsersSelector = (state: AppStateType) => {
     return state.usersPage.users;
 }
 
-export const getPageSize=(state:AppStateType)=>{
+//                                    вставляем юзерсов    передаем в колбэк результат юзерсов
+export const GetUsers = createSelector(GetUsersSelector, (users: Array<usersType>) => {
+    return users.filter(f => true);
+})
+
+export const getPageSize = (state: AppStateType) => {
     return state.usersPage.pageSize;
 }
 
-export const getTotalUsersCount=(state:AppStateType)=>{
+export const getTotalUsersCount = (state: AppStateType) => {
     return state.usersPage.totalUsersCount;
 }
 
-export const getCurrentPage=(state:AppStateType)=>{
+export const getCurrentPage = (state: AppStateType) => {
     return state.usersPage.currentPage;
 }
 
-export const getIsFetching=(state:AppStateType)=>{
+export const getIsFetching = (state: AppStateType) => {
     return state.usersPage.isFetching;
 }
 
-export const getFollowingInProgress=(state:AppStateType)=>{
+export const getFollowingInProgress = (state: AppStateType) => {
     return state.usersPage.followingInProgress;
 }
 
